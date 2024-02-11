1db65bb8f0350dd8741da575153f88e1fd48ae51
Arnabbndc <1905065@ugrad.cse.buet.ac.bd>
2024-02-11 22:23:45 +0600

    Components and hooks merged

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import './index.css'
-import {ThemeProvider} from '@mui/material'
-import {createTheme} from "@mui/material/styles"
-import Home from './Pages/Home'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CartContextProvider from "./Hooks/CartContextProvider";
+import "./index.css";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import Home from "./Pages/Home";
+import Appbar from "./Components/Appbar";
 
 
-const customTheme=createTheme({
-  palette:{
- 
-  },
+const customTheme = createTheme({
+  palette: {},
   typography: {
     fontFamily: [
       "Oswald",
-      '-apple-system',
-      'BlinkMacSystemFont',
+      "-apple-system",
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
+      "Arial",
+      "sans-serif",
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
-    ].join(','),
+    ].join(","),
   },
-
-})
+});
 
 function App() {
-  
-
-  
-
-  return(
+  return (
     <BrowserRouter>
-    
+      <CartContextProvider>
         <ThemeProvider theme={customTheme}>
           <Switch>
-            <Route path="/" exact component={Home}/>
+            <Route path="/" exact component={Home} />
           </Switch>
-          
+          <Appbar />
         </ThemeProvider>
-      
+      </CartContextProvider>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
